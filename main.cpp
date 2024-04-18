e61805ddf933d101f3427b68cff9576e5669d5a4
EricHanJf <jf08942378268@gmail.com>
2024-04-18 11:09:13 +0100

    Added the menu in main app and now try to do the next feature "Tap the Bug Board".

--- main.cpp
+++ main.cpp
@@ -4,16 +4,42 @@ using namespace std;
 
 int main() {
     board board;
+    int numberChoice;
+    bool exit = false;
     board.intializeBugs("bugs.txt");
-    board.displayAllBugs();
+    while(!exit){
+        cout << " =============================================" << endl;
+        cout << " |           Bug's Life Feature Menu         |" << endl;
+        cout << " =============================================" << endl;
+        cout << " |           [1]. Display All Bugs           |" << endl;
+        cout << " |           [2]. Display Bug By ID          |" << endl;
+        cout << " |           [0]. Exit The Program           |" << endl;
+        cout << " =============================================\n" << endl;
 
-    int BugId;
-    cout << "Please enter a Bug Id to search (eg. 101,102...)";
-    cin >> BugId;
-    board.findAndDisplayBug(BugId);
+        cout << "Please choice any number you want." << endl;
+        cin >> numberChoice;
+        switch (numberChoice) {
+            case 1:
+                board.displayAllBugs();
+                break;
+            case 2:
+                int BugId;
+                cout << "Please enter a Bug Id to search (eg. 101,102...)";
+                cin >> BugId;
+                board.findAndDisplayBug(BugId);
+                break;
+            case 0:
+                exit = true;
+                cout << "Exiting the program..." << endl;
+                break;
+            default:
+                cout<<"invalid choice, please try it again" << endl;
+                break;
+        }
+    }
+
+
     return 0;
 
-//    int BugId;
-//    cout << "Please enter a Bug Id to search (eg. 101,102...)";
-//    cin >> BugId;
+
 }
